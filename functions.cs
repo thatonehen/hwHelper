998c9823e92401ce6fe76ee3cba540cd5383c588
Henry <91961647+thatonehen@users.noreply.github.com>
2022-11-11 17:33:22 -0500

    Moved the functions class to a seperate file

---
+++ functions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkHelper
+{
+    class functions
+    {
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public static int Solve(int num1, int num2, string op)
+        {
+            switch (op)
+            {
+                case "*":
+                    return Multiply(num1, num2);
+
+                case "x":
+                    return Multiply(num1, num2);
+
+                case "/":
+                    return Divide(num1, num2);
+
+                case "-":
+                    return Subtract(num1, num2);
+
+                case "+":
+                    return Add(num1, num2);
+
+                default:
+                    return 0;
+            }
+        }
+    }
+}
