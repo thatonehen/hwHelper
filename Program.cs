4af91fe23f70c19c0cd772a562f5113c3193c744
Henry <91961647+thatonehen@users.noreply.github.com>
2022-11-11 17:33:56 -0500

    removed functions, moved them to a seperate file.

--- Program.cs
+++ Program.cs
@@ -3,49 +3,6 @@ using System.IO;
 using System.Threading;
 namespace checkWorkHW
 {
-
-    class functions
-    {
-        public static int Multiply(int num1, int num2)
-        {
-            return num1 * num2;
-        }
-        public static int Divide(int num1, int num2)
-        {
-            return num1 / num2;
-        }
-        public static int Add(int num1, int num2)
-        {
-            return num1 + num2;
-        }
-        public static int Subtract(int num1, int num2)
-        {
-            return num1 - num2;
-        }
-        public static int Solve(int num1, int num2, string op)
-        {
-            switch (op)
-            {
-                case "*":
-                    return Multiply(num1, num2);
-
-                case "x":
-                    return Multiply(num1, num2);
-
-                case "/":
-                    return Divide(num1, num2);
-
-                case "-":
-                    return Subtract(num1, num2);
-
-                case "+":
-                    return Add(num1, num2);
-
-                default:
-                    return 0;
-            }
-        }
-    }
     class Program
     {
         
